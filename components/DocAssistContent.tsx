134d162c1a656e59bfab1fbd63b88f7689281605
IAmStevenHale <iamstevenhale@gmail.com>
2023-01-09 13:54:02 +1000

    desktop complete

--- components/DocAssistContent.tsx
+++ components/DocAssistContent.tsx
@@ -12,41 +12,29 @@ import ContentProps, { LineTextProps, ShowCaseContainer } from '../Interfaces';
  * elements within the component. */
 const DocAssistContent: React.FC<ContentProps> = ({ currentIndex, thisIndex }) => {
     const isCurrent = currentIndex === thisIndex;
-    const [fontChanged, setFontChanged] = useState(false);
 
-    useEffect(() => {
-        if (isCurrent) {
-            setFontChanged(true);
-        }
-    }, [isCurrent]);
-
-    const text = "DocAssist is a web-based tool that helps Allied Health Professionals find relevant MBS rebates for their patients. The application utilizes a Rake Filtering algorithm to identify key terms in patient notes and generate a list of relevant MBS items. The application is built with a modern tech stack including React, TypeScript, and styled components. This allows for efficient and reliable performance, as well as a visually appealing user interface. DocAssist streamlines the process of finding MBS rebates, saving time and effort for Allied Health Professionals. Give it a try and see how it can benefit your practice.";
     return (
         <Wrapper>
-            <Gradient/>
-                <ShowCaseContainerLeft isCurrent={isCurrent}>
-                    <h2>DocAssist</h2>
-                    <br />
-                    <p>{text}</p>
-                    <br/>
-                    <p><a target={'_blank'} rel="noreferrer" href='https://www.docassist.org/'>https://www.docassist.org/</a></p>
-                </ShowCaseContainerLeft>
-                <ShowCaseContainerRight isCurrent={isCurrent}>
-                    <Image src='/docAssist.png' alt='A landscape ipad showcasing the Dicefall app.' width={543} height={649} />
-                </ShowCaseContainerRight>
+            <Gradient />
+            <ShowCaseContainerLeft isCurrent={isCurrent}>
+                <h2>DocAssist</h2>
+                <br />
+                <p>
+                    DocAssist is a web-based tool that helps Allied Health Professionals find relevant MBS rebates for their patients.
+                    The application utilizes a Rake Filtering algorithm to identify key terms in patient notes and generate a list of
+                    relevant MBS items. The application is built with a modern tech stack including React, TypeScript, and styled components.
+                    This allows for efficient and reliable performance, as well as a visually appealing user interface. DocAssist
+                    streamlines the process of finding MBS rebates, saving time and effort for Allied Health Professionals. Give it a
+                    try and see how it can benefit your practice.
+                </p>
+                <br />
+                <p><a target={'_blank'} rel="noreferrer" href='https://www.docassist.org/'>https://www.docassist.org/</a></p>
+            </ShowCaseContainerLeft>
+            <ShowCaseContainerRight isCurrent={isCurrent}>
+                <Image src='/docAssist.png' alt='A landscape ipad showcasing the Dicefall app.' width={543} height={649} />
+            </ShowCaseContainerRight>
         </Wrapper>
     );
-};
-
-const TextBlock = ({ text, fontChanged, index }: LineTextProps) => {
-    const [fontChangedLocal, setFontChangedLocal] = useState(false);
-    useEffect(() => {
-        setTimeout(() => {
-            setFontChangedLocal(fontChanged);
-        }, 100 * index);
-    }, [fontChanged, index]);
-
-    return fontChangedLocal ? <NewFont>{text}</NewFont> : <OldFont>{text}</OldFont>;
 };
 
 const Wrapper = styled.div`
@@ -66,7 +54,7 @@ const Gradient = styled.div`
     radial-gradient(ellipse 50% 30% at 100% 0%,#25bec32d,var(--transparent)),
     radial-gradient(ellipse 50% 30% at 0% 100%,#25bec32d,var(--transparent)),
     radial-gradient(ellipse 50% 30% at 100% 100%,#25bec32d,var(--transparent));
-        position: absolute;
+    position: absolute;
     height: 100%;
     width: 100%;
 `;
@@ -89,6 +77,7 @@ const ShowCaseContainerRight = styled.div<ShowCaseContainer>`
         -webkit-filter: drop-shadow(10px 10px 15px #000000b2);
         transition: 1.2s ease-in-out;
     }
+
     //MOBILE
     @media (max-width: 600px) {
         width: 100%;
@@ -115,7 +104,7 @@ const ShowCaseContainerLeft = styled.div<ShowCaseContainer>`
         font-size: 30px;
     }
 
-        //TABLET
+    //TABLET
     @media (max-width: 900px) {
         width: 100%;
         height: 100%;
@@ -128,35 +117,6 @@ const ShowCaseContainerLeft = styled.div<ShowCaseContainer>`
         height: 60%;
         padding: 0 30px;
         justify-content: flex-start;
-    }
-`;
-
-const TextContainer = styled.div`
-    white-space: pre-line;
-    text-align: center;
-`;
-
-const NewFont = styled.p`
-    font-size: 15px;
-    font-family: "Linear","SF Pro Display",-apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,Oxygen,Ubuntu, Cantarell,"Open Sans","Helvetica Neue",sans-serif; 
-    transition: font-family 0.5s ease-in-out;
-    display: inline-block;
-    line-height: 28px;
-    overflow: hidden;
-     @media (max-width: 600px) {
-        font-size: calc(14px + (24 - 14) * (100vmin - 280px) / (1200 - 280));
-    }
-`;
-
-const OldFont = styled.p`
-    font-size: 10px;
-    font-family: 'Meddon', cursive !important;
-    transition: font-family 0.5s ease-in-out;
-    display: inline-block;
-    line-height: 28px;
-    overflow: hidden;
-     @media (max-width: 600px) {
-        font-size: calc(14px + (24 - 14) * (100vmin - 280px) / (1200 - 280));
     }
 `;
 
