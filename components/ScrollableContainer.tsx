6ff1c87b2da60deed45a4bf0de185ff9590af20e
IAmStevenHale <iamstevenhale@gmail.com>
2023-01-12 12:27:21 +1000

    test new scroll event listener

--- components/ScrollableContainer.tsx
+++ components/ScrollableContainer.tsx
@@ -34,10 +34,10 @@ const ScrollableContainer: React.FC<ScrollableProps> = ({ currentIndex, setCurre
         };
         
         window && window.addEventListener('scroll', handleIndex);
-        currentRef && currentRef.addEventListener('scroll', handleIndex);
+        // currentRef && currentRef.addEventListener('scroll', handleIndex);
         return () => {
             window && window.removeEventListener('scroll', handleIndex);
-            currentRef && currentRef.removeEventListener('scroll', handleIndex);
+            // currentRef && currentRef.removeEventListener('scroll', handleIndex);
         };
     }, [children.length, currentIndex, ref, setCurrentIndex]);
 
